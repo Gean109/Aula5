1fef95fda0b98db2a82f942b4bed0efc56415fea
Gean109 <142437784+Gean109@users.noreply.github.com>
2023-08-21 17:34:55 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+/*
+* Primeiro programa em JavaScript
+*Gean Carlos
+*/
+
+console.log("Olá mundo!");
+//prompt("Qual é o seu nome");
+const VariavelA = 100;
+const VariavelB = VariavelA + 50;
+const VariavelC = VariavelA / VariavelB;
+console.log(VariavelA);
+console.log(VariavelB);
+console.log(VariavelC);
+
+
+//Variavel let
+let numero = 5
+console.log(numero);
+numero = 120;
+console.log(numero);
+
+//Concatenar textos
+let nome = "Gean";
+let sobrenome = "Carlos";
+let idade = 16;
+console.log("Olá", nome, sobrenome, "voce tem", idade, "anos");
+
+//booleanas
+const variavelFalsa = false;
+const variavelVerdadeira = true;
+
+console.log ("Variável falsa:" + variavelFalsa);
+console.log("Variável verdadeira: " + variavelVerdadeira);
+
+
+//Exercicio
+
+
+
+
+//typeof
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof estudante);
+
+
+let banana;
+console.log(typeof banana);
+banana = "fruta";
+
+
+
+
+//PROMPT
+
+let nome2 = prompt ("seu nome");
+let idade2 = prompt ("idade");
+comsole.log(typeof nome2);
+console.log(typeof idade2);
